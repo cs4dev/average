690e9fb2749cc183df3d3622c5d8e50d1cbc9206
Jon Lim <jonlim@Jons-MacBook-Pro.local>
2023-02-10 18:58:33 +0800

    add datafile

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 // average calculate the average of serveral numbers
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/headfirstgo/datafile"
+)
 
 func main() {
-	numbers := [3]float64{71.8, 56.2, 89.5}
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 	var sum float64 = 0
 	for _, number := range numbers {
 		sum += number
